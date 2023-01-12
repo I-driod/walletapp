32ab68c814f3dade4426e72c96dc34edf787af27
Godwin David <oneandtrueblueking@gmail.com>
2023-01-12 14:14:13 +0100

    Initial commit

---
+++ lib/util/my_button.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String icon;
+  final String text;
+
+  const MyButton({Key? key,
+    required this.icon,
+    required this.text
+
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          children: [
+            Container(
+              height: 80,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.grey[100],
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade400,
+                    blurRadius: 40,
+                    spreadRadius: 10,),
+                ],
+              ),
+              child: Center(
+                child: Image.asset('icons/'+ icon),
+              ),
+            ),
+            SizedBox(height: 10,),
+            Text( text,
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[700]
+              ),)
+          ],
+        )
+      ],
+    );
+  }
+}
